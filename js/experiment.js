6d69863f09c480b0e2f3928177bfec253190cf56
zwlzwt <zwlzwt@zwlzwtdeMacBook-Pro.local>
2015-06-29 18:49:13 +0800

    main page modified

--- js/experiment.js
+++ js/experiment.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 			$formSignup = $loginModal_container.find('.signup-form'),
 			$modalTitle_switcher = $('.modal-title-switcher'),
 			$tabLogin = $modalTitle_switcher.children('li').eq(0).children('a'),
-			$tabSignup = $modalTitle_switcher.children('li').eq(1).children('a');
+			$tabSignup = $modalTitle_switcher.children('li').eq(1).children('a'),
+			$answerVotelist = $('.main-content-container').find('.content-innerblock-list'),
+			$backTotop = $('.back-to-top');
 
 	/* 搜索开关 */
 	$('.search-button').on('click',function(event){
@@ -86,8 +88,10 @@ $(document).ready(function(){
   $modalTitle_switcher.on('click',function(){
   	($(event.target).is($tabLogin)) ? login_selected() : signup_selected();
   });
-
-
+ /* 移动端登录注册界面 */
+ $('.menu-top-bar').find('.main-nav').on('click',function(){
+ 		$('.login-nav').toggleClass('is-visible');
+ });
 
 
 
@@ -135,6 +139,55 @@ $(document).ready(function(){
 		
 		( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
 		( 'Hide' == $this.text() ) ? $this.text('Show') : $this.text('Hide');
+	});
+
+/* 主页内容显示 */
+	function visibleContent(e){
+		$answerVotelist.eq(e).find('.content-summary').css('display','none');
+		$answerVotelist.eq(e).find('.editable-content').fadeIn(800);
+		$answerVotelist.eq(e).children('.feedback-footer').children('span').css('visibility','visible');
+	}
+	function hideContenclose(e){
+		$answerVotelist.eq(e).find('.content-summary').css('display','block');
+		$answerVotelist.eq(e).children('.feedback-footer').children('span').css('visibility','hidden');
+		$answerVotelist.eq(e).find('.editable-content').css('display','none');
+	}	
+	$('.content-summary').children('span').on('click',function(){
+		var lisNum = $(this).parent().parent().parent().index();
+		visibleContent(lisNum);
+	});
+	$('.feedback-footer').children('span').on('click',function(){
+		var lisNum = $(this).parent().parent().index();
+		hideContenclose(lisNum);
+	});
+/* 点赞 */
+	function addVotenumber(e){
+		$answerVotelist.eq(e).find('.vote-button-count').text(function(){
+			return Number($(this).text()) + 1;
+		});
+	}
+	$('.answer-vote').find('.up').on('click',function(){
+		 var lisNum = $(this).parent().parent().parent().index();
+		 addVotenumber(lisNum);
+		});
+
+/* 回到顶部 */
+	var offset = 300,
+		  offset_opacity = 1200,
+		  scroll_top_duration = 700;
+
+	$(window).scroll(function(){
+		($(this).scrollTop() > offset) ? $backTotop.addClass('is-visible') : $backTotop.removeClass('is-visible');
+		if($(this).scroll() > offset_opacity){
+			$backTotop.addClass('fade-out');
+		}
+	});
+	$backTotop.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
 	});
 
 });
